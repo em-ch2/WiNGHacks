f0ba589a369d552cd955c5d1b7102419ec7e4728
Leonna Xie <197978858+koicaaaa@users.noreply.github.com>
2026-02-21 03:11:04 -0500

    Working on transition functionality

---
+++ wing-hacks/scripts/global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var current_scene = "world"
+var transition_scene = false
+
+var player_exit_palace_posx = 0
+var player_exit_palace_posy = 0
+
+var player_start_posx = 0
+var player_start_posy = 0
