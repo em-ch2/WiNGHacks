72a95440151b0a6eb37ddd6637314e4d2dfef789
Leonna Xie <197978858+koicaaaa@users.noreply.github.com>
2026-02-21 08:52:09 -0500

    Transition into scene 2

--- wing-hacks/scripts/scene_2.gd
+++ wing-hacks/scripts/scene_2.gd
@@ -3,13 +3,27 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var player = $Player
+	var camera = player.get_node("player/Camera2D")
+	var tilemap = $TileMapLayer
+	
+	var used_rect = tilemap.get_used_rect()
+	var tile_size = tilemap.tile_set.tile_size
+	
+	var map_pixel_size = used_rect.size * tile_size
+	var map_pixel_position = used_rect.position * tile_size
+	
+	camera.limit_left = map_pixel_position.x
+	camera.limit_top = map_pixel_position.y
+	camera.limit_right = map_pixel_position.x + map_pixel_size.x
+	camera.limit_bottom = map_pixel_position.y + map_pixel_size.y
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
+	
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		get_tree().change_scene_to_file("res://scene/world.tscn")
