d00e5acfd321860c6c22a517c7d10fe6b318172c
Leonna Xie <197978858+koicaaaa@users.noreply.github.com>
2026-02-21 12:37:56 -0500

    Added dumpling game

--- wing-hacks/scripts/global.gd
+++ wing-hacks/scripts/global.gd
@@ -8,3 +8,8 @@ var player_exit_palace_posy = 0
 
 var player_start_posx = 0
 var player_start_posy = 0
+
+var offered_dumplings = false
+func start_dumplings_minigame() -> void:
+	print("starting minigame")
+	get_tree().call_deferred("change_scene_to_file", "res://scene/minigame.tscn")
