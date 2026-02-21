3817bff27b7f1b69c1e9c51e5ee5cf01007be80a
Leonna Xie <197978858+koicaaaa@users.noreply.github.com>
2026-02-21 08:36:29 -0500

    Fixed scope and working on transitions

--- wing-hacks/scripts/scene_1.gd
+++ wing-hacks/scripts/scene_1.gd
@@ -3,7 +3,21 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var player = $Player
+	var camera = player.get_node("player/Camera2D")
+	var tilemap = $TileMapLayer
+	
+	var used_rect = tilemap.get_used_rect()
+	var tile_size = tilemap.tile_set.tile_size
+	
+	var map_pixel_size = used_rect.size * tile_size
+	var map_pixel_position = used_rect.position * tile_size
+	
+	camera.limit_left = map_pixel_position.x
+	camera.limit_top = map_pixel_position.y
+	camera.limit_right = map_pixel_position.x + map_pixel_size.x
+	camera.limit_bottom = map_pixel_position.y + map_pixel_size.y
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
