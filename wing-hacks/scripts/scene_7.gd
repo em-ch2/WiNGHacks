5bbbcd3893a4dec93c3886ff9d590caec4f72e2d
Leonna Xie <197978858+koicaaaa@users.noreply.github.com>
2026-02-21 22:12:41 -0500

    Fixing music player

--- wing-hacks/scripts/scene_7.gd
+++ wing-hacks/scripts/scene_7.gd
@@ -19,7 +19,6 @@ func _ready() -> void:
 	camera.limit_bottom = map_pixel_position.y + map_pixel_size.y
 	
 	Music.player.stream = preload("res://assets/Dragon Dance - Loop.wav")
-	player.volume_db = -6
 	Music.player.play()
 
 func _unhandled_input(_event: InputEvent) -> void:
